acf86052b5b1b08b7939a6f6595b1715165eac0b
Brenno Ferreira de Melo <brenno.melo@sptech.school>
2026-06-25 11:40:28 -0300

    Atualizando o script-tabelas.sql

--- Music_Io/src/database/script-tabelas.sql
+++ Music_Io/src/database/script-tabelas.sql
@@ -6,57 +6,35 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE musicIo;
 
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+USE musicIo;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(90) UNIQUE,
+    senha VARCHAR(20)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE quiz (
+	idQuizTentativa INT PRIMARY KEY AUTO_INCREMENT,
+    fkUsuario INT,
+		CONSTRAINT ctFkUsuario
+			FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+	pontuacao_total INT,
+    dtHora DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE pontuacaoMateriasQuiz (
+	idMateria INT PRIMARY KEY AUTO_INCREMENT,
+    fkQuizTentativa INT,
+		CONSTRAINT ctFkfkQuizTentativa
+			FOREIGN KEY (fkQuizTentativa) REFERENCES quiz(idQuizTentativa),
+	m1 INT,
+	m2 INT,
+	m3 INT,
+	m4 INT,
+	m5 INT,
+	m6 INT
 );
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
